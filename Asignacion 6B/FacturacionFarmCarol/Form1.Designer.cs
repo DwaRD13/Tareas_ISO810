019f8b911ef9bfdb9dd904fbeab90489e576a738
DwaRD13 <munozdarwin013@gmail.com>
2026-02-22 08:30:18 -0400

    Creacion del Windows Form con el logo de la empresa

---
+++ Asignacion 6B/FacturacionFarmCarol/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace FacturacionFarmCarol
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            imageList1 = new ImageList(components);
+            pictureBox1 = new PictureBox();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            tituloApp = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // imageList1
+            // 
+            imageList1.ColorDepth = ColorDepth.Depth32Bit;
+            imageList1.ImageSize = new Size(16, 16);
+            imageList1.TransparentColor = Color.Transparent;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(267, 18);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(227, 99);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(panel2);
+            panel1.Location = new Point(12, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(791, 546);
+            panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(tituloApp);
+            panel2.Controls.Add(pictureBox1);
+            panel2.Location = new Point(12, 6);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(762, 146);
+            panel2.TabIndex = 1;
+            panel2.Paint += panel2_Paint;
+            // 
+            // tituloApp
+            // 
+            tituloApp.AutoSize = true;
+            tituloApp.Location = new Point(338, 125);
+            tituloApp.Name = "tituloApp";
+            tituloApp.Size = new Size(69, 15);
+            tituloApp.TabIndex = 1;
+            tituloApp.Text = "Facturación";
+            tituloApp.Click += label1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(815, 593);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ImageList imageList1;
+        private PictureBox pictureBox1;
+        private Panel panel1;
+        private Panel panel2;
+        private Label tituloApp;
+    }
+}
